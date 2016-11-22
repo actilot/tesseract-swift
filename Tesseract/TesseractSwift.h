f5bcad5c4c7842482e8bdd756119bc87bdeaab87
Axel Campaña <axel@softwarewot.com>
2016-11-23 01:14:01 +0800

    Added Tesseract.swift and Tesseract.h

---
+++ Tesseract/TesseractSwift.h
@@ -0,0 +1,16 @@
+//
+//  Macros.swift
+//  Tesseract
+//
+//  Created by Axel  Campaña on 11/22/16.
+//  Copyright © 2016 Axel Campaña. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Auth0.
+FOUNDATION_EXPORT double TesseractSwiftVersionNumber;
+
+//! Project version string for Auth0.
+FOUNDATION_EXPORT const unsigned char TesseractSwiftVersionString[];
+
---
+++ Tesseract/TesseractSwift.swift
@@ -0,0 +1,10 @@
+//
+//  Macros.swift
+//  Tesseract
+//
+//  Created by Axel  Campaña on 11/22/16.
+//  Copyright © 2016 Axel Campaña. All rights reserved.
+//
+
+import Macros
+
