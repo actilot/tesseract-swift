c302f0787374e5b927c42e90153184ec57a7511a
Axel Campaña <axel@softwarewot.com>
2016-11-23 09:57:24 +0800

    Change project to framework

---
+++ Tesseract/Tesseract/Tesseract.h
@@ -0,0 +1,19 @@
+//
+//  Tesseract.h
+//  Tesseract
+//
+//  Created by Axel Ros Campaña on 11/23/16.
+//  Copyright © 2016 Axel Ros Campaña. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Tesseract.
+FOUNDATION_EXPORT double TesseractVersionNumber;
+
+//! Project version string for Tesseract.
+FOUNDATION_EXPORT const unsigned char TesseractVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Tesseract/PublicHeader.h>
+
+
